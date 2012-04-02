d7b40b4736c62930d920799c88cc20ff556215ba
Amanda Fenech <amanda.fenech@marsec-xl.org>
2012-04-02 11:49:03 +0200

    updated logger

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php session_start();?>
 <?php include ("includes/dataclass.php");?>
+<?php include ("js/data.php");?>
 
 <?php
 if ( isset($_GET['sortvalue']) )
@@ -19,32 +20,6 @@ else
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-<script language="javascript" type="text/javascript"
-	src="datetimepicker.js"></script>
-<style type="text/css">
-h1 {
-	color: #00ff00;
-}
-
-td {
-	font-family: Arial, Verdana;
-	font-size: 15px;
-	background: #F9F8FB;
-	color: #716E6E;
-	font-weight: normal;
-	/* text-align: center; */
-	vertical-align: middle;
-}
-
-th {
-	background: #CFDBF3;
-	font-weight: bold;
-	color: #445276;
-	font-size: 17px;
-}
-</style>
-
-
 <title>Home</title>
 
 
---
+++ js/data.php
@@ -0,0 +1,27 @@
+<?php ?>
+<style type="text/css">
+h1 {
+	color: #00ff00;
+}
+
+td {
+	font-family: Arial, Verdana;
+	font-size: 15px;
+	background: #F9F8FB;
+	color: #716E6E;
+	font-weight: normal;
+	/* text-align: center; */
+	vertical-align: middle;
+}
+
+th {
+	background: #CFDBF3;
+	font-weight: bold;
+	color: #445276;
+	font-size: 17px;
+}
+</style>
+
+
+<script language="javascript" type="text/javascript"
+	src="datetimepicker.js"></script>
